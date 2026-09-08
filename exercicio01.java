416b6748b2f389f465e206e1a57aed856f9b0375
makiwilly <maximus.resende@aluno.senai.br>
2026-09-08 17:15:36 -0300

    Add files via upload
    tipos primitivos

---
+++ exercicio01.java
@@ -0,0 +1,13 @@
+// tipos primitivos e variaveis!
+
+class Main {
+    public static void main(String[] args) {
+        String nome = "Max";
+        String endereco = "Rua dos passaros";
+        String data = "08/08/2026";
+        float salario = 800;
+        
+
+        System.out.println("EU " + nome + ", morando no endereco " + endereco + ", recebi o meu salario de: " + salario + " Na data " + data);
+    }
+}
